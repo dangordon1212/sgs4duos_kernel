aee8f579cb9352922e0dbac5a3b084595e96296f
Yuanjie Huang <huangyuanjie@gmail.com>
2013-10-13 01:53:28 +0800

    MDE

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Apr 12 17:01:16 KST 2013"
+#define LINUX_COMPILE_BY "hslu.sim"
+#define LINUX_COMPILE_HOST "AUDIO"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ make_kernel.sh
@@ -0,0 +1,2 @@
+make ARCH=arm ja3gduos_chn_cu_00_defconfig
+make ARCH=arm
