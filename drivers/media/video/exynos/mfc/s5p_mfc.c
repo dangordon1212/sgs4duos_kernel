7402bcf26a5a40e697721a4c9c331b711558eb7d
Yuanjie Huang <huangyuanjie@gmail.com>
2013-10-13 03:47:06 +0800

    cleanup and fix

--- drivers/media/video/exynos/mfc/s5p_mfc.c
+++ drivers/media/video/exynos/mfc/s5p_mfc.c
@@ -200,7 +200,7 @@ void mfc_sched_worker(struct work_struct *work)
 		mfc_err("no mfc device to run\n");
 }
 
-inline int clear_hw_bit(struct s5p_mfc_ctx *ctx)
+int clear_hw_bit(struct s5p_mfc_ctx *ctx)
 {
 	struct s5p_mfc_dev *dev = ctx->dev;
 	int ret = -1;
@@ -213,7 +213,7 @@ inline int clear_hw_bit(struct s5p_mfc_ctx *ctx)
 
 /* Helper functions for interrupt processing */
 /* Remove from hw execution round robin */
-inline void clear_work_bit(struct s5p_mfc_ctx *ctx)
+void clear_work_bit(struct s5p_mfc_ctx *ctx)
 {
 	struct s5p_mfc_dev *dev = NULL;
 
